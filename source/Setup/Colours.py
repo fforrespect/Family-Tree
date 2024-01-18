dea06238d428ab41079a22b8b7c0b985f5fa1b11
Jaiveer Chadda <jaiveer.chadda@gmail.com>
2024-01-18 11:12:43 +0000

    Added colours file

---
+++ source/Setup/Colours.py
@@ -0,0 +1,17 @@
+BLACK  : tuple[int, int, int] = (  0,   0,   0)  #
+BLUE   : tuple[int, int, int] = (  0,   0, 255)  #
+CYAN   : tuple[int, int, int] = (  0, 255, 255)
+D_GRAY : tuple[int, int, int] = ( 40,  40,  40)
+FUCHSIA: tuple[int, int, int] = (255,   0, 255)
+GRAY   : tuple[int, int, int] = (128, 128, 128)
+GREEN  : tuple[int, int, int] = (  0, 255,   0)  #
+LIME   : tuple[int, int, int] = (  0, 255,   0)
+MAROON : tuple[int, int, int] = (128,   0,   0)
+NAVY   : tuple[int, int, int] = (  0,   0, 128)
+OLIVE  : tuple[int, int, int] = (128, 128,   0)
+PURPLE : tuple[int, int, int] = (128,   0, 128)
+RED    : tuple[int, int, int] = (255,   0,   0)
+SILVER : tuple[int, int, int] = (192, 192, 192)
+TEAL   : tuple[int, int, int] = (  0, 128, 128)
+WHITE  : tuple[int, int, int] = (255, 255, 255)
+YELLOW : tuple[int, int, int] = (255, 255,   0)
